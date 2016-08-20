d04e004919af1c58f3c9e40548ef5dd7202217d5
Colin James Dolese <colin@DN51vkbu.SUNet>
2016-08-19 22:57:30 -0700

    Network Hooked Up

---
+++ Agrity/LaunchExtensionView.swift
@@ -0,0 +1,13 @@
+//
+//  LaunchExtensionView.swift
+//  Agrity
+//
+//  Created by Colin James Dolese on 8/19/16.
+//  Copyright © 2016 Agrity. All rights reserved.
+//
+
+import Cocoa
+
+class LaunchExtensionView: UIViewController {
+
+}
